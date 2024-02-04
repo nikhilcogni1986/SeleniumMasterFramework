a7e8f3ebbc4257bc83172785bddaa2e1c98cad33
Nikhil <nikhilcogni@gmail.com>
2024-02-04 10:23:52 +0000

    Removed unsued imports

--- src/test/java/pom/base/BaseTest.java
+++ src/test/java/pom/base/BaseTest.java
@@ -1,11 +1,18 @@
 package pom.base;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import pom.factory.DriverManager;
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+
+import java.io.File;
+import java.io.IOException;
 
 public class BaseTest
 {
@@ -31,8 +38,20 @@ public class BaseTest
     }
 
     @AfterMethod
-    public synchronized void quitDriver()
-    {
+    public synchronized void quitDriver(ITestResult result, @Optional String browser) throws IOException {
+        if(result.getStatus() == ITestResult.FAILURE)
+        {
+            File destFile = new File("scr" + File.separator+
+                    browser+File.separator+result.getTestClass().getRealClass().getSimpleName()+
+                    "_"+result.getMethod().getMethodName()+".png");
+            takeScreenshot(destFile);
+        }
         getDriver().quit();
+    }
+
+    private void takeScreenshot(File destFile) throws IOException, IOException {
+        TakesScreenshot ts = (TakesScreenshot)getDriver();
+        File srcFile = ts.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyDirectory(srcFile, destFile);
     }
 }
